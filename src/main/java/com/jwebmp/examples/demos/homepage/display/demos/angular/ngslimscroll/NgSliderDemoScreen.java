98b84e771421a4c5b1c3fd5b8acaa2539d0d1bc6
GedMarc <ged_marc@hotmail.com>
2018-06-07 01:59:51 +0200

    Bom Implementation, Demo Screens Base

---
+++ src/main/java/com/jwebmp/examples/demos/homepage/display/demos/angular/ngslimscroll/NgSliderDemoScreen.java
@@ -0,0 +1,12 @@
+package com.jwebmp.examples.demos.homepage.display.demos.angular.ngslimscroll;
+
+import com.jwebmp.examples.demos.homepage.components.general.PluginDemoScreen;
+
+public class NgSliderDemoScreen
+		extends PluginDemoScreen
+{
+	public NgSliderDemoScreen()
+	{
+		super("NG Slimscroll", "Angular", "NG Slim Scroll");
+	}
+}
---
+++ src/main/java/com/jwebmp/examples/demos/homepage/display/demos/display/moment/MomentDemoScreen.java
@@ -0,0 +1,12 @@
+package com.jwebmp.examples.demos.homepage.display.demos.display.moment;
+
+import com.jwebmp.examples.demos.homepage.components.general.PluginDemoScreen;
+
+public class MomentDemoScreen
+		extends PluginDemoScreen
+{
+	public MomentDemoScreen()
+	{
+		super("Moment", "Display", "Moment Dates");
+	}
+}
