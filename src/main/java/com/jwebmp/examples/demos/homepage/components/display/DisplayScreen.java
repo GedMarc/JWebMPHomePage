11bc0b8e675f7c70d68d90995c7b7f1a99378c98
GedMarc <ged_marc@hotmail.com>
2018-08-11 13:17:12 +0200

    Better control on the ajax responses

--- src/main/java/com/jwebmp/examples/demos/homepage/components/display/DisplayScreen.java
+++ src/main/java/com/jwebmp/examples/demos/homepage/components/display/DisplayScreen.java
@@ -2,28 +2,16 @@ package com.jwebmp.examples.demos.homepage.components.display;
 
 import com.jwebmp.core.Page;
 import com.jwebmp.core.base.ajax.AjaxResponse;
-import com.jwebmp.core.base.html.*;
+import com.jwebmp.core.base.html.Div;
+import com.jwebmp.core.base.html.H4;
 import com.jwebmp.examples.demos.homepage.components.DefaultSlimScroll;
 import com.jwebmp.guicedinjection.GuiceContext;
 import com.jwebmp.plugins.angularslimscroll.SlimScrollFeature;
 import com.jwebmp.plugins.bootstrap4.breadcrumbs.BSBreadCrumb;
 import com.jwebmp.plugins.bootstrap4.cards.parts.BSCardBody;
-import com.jwebmp.plugins.bootstrap4.collapse.BSCollapse;
 import com.jwebmp.plugins.bootstrap4.containers.BSContainer;
-import com.jwebmp.plugins.bootstrap4.options.BSClearfixOptions;
-import com.jwebmp.plugins.bootstrap4.options.BSColumnOptions;
-import com.jwebmp.plugins.fontawesome5.FontAwesome;
-import com.jwebmp.plugins.fontawesome5.FontAwesomeList;
-import com.jwebmp.plugins.fontawesome5.IFontAwesomeIcon;
-import com.jwebmp.plugins.google.sourceprettify.JQSourceCodePrettify;
-import com.jwebmp.plugins.google.sourceprettify.SourceCodeLanguages;
-import io.undertow.util.FileUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
 
 import javax.validation.constraints.NotNull;
-import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public abstract class DisplayScreen<J extends DisplayScreen<J>>
 		extends DisplayPart<J>
@@ -43,6 +31,48 @@ public abstract class DisplayScreen<J extends DisplayScreen<J>>
 
 	}
 
+	@Override
+	public void preConfigure()
+	{
+		if (!isConfigured())
+		{
+			setID("content-updatable");
+			addStyle("background-color:#333");
+			addStyle("padding:0px;");
+
+			Div d = getContentContainer();
+			d.setID("content-updatable-content");
+			if (!getPage().isMobileOrSmartTablet())
+			{
+				d.addStyle("margin-top:15px");
+			}
+
+			BSCardBody body = addCardBody().add(d);
+			body.addStyle("padding:0px;");
+
+			SlimScrollFeature destroyFeature = new SlimScrollFeature(this).setSortOrder(40);
+			destroyFeature.getOptions()
+			              .setDestroy(true);
+			addFeature(destroyFeature);
+
+			addFeature(new DefaultSlimScroll(this).setSortOrder(45));
+
+			d.setID("innerContentContainer");
+			if (getPage().isMobileOrSmartTablet())
+			{
+				d.addStyle("padding-left", "0px");
+				d.addStyle("padding-right", "0px");
+			}
+
+			Div slimScrollerSpacer = new Div<>().setID("slimScrollSpacer");
+			slimScrollerSpacer.addStyle("height:70px;");
+			d.add(slimScrollerSpacer);
+
+			buildTitleRow();
+		}
+		super.preConfigure();
+	}
+
 	@NotNull
 	public abstract BSContainer<?> getContentContainer();
 
@@ -50,7 +80,7 @@ public abstract class DisplayScreen<J extends DisplayScreen<J>>
 	{
 		Page page = GuiceContext.getInstance(Page.class);
 		Div responsive = new Div<>().setID("innerNorth");
-		if(!page.isMobileOrSmartTablet())
+		if (!page.isMobileOrSmartTablet())
 		{
 			Div pageTitleBox = new Div();
 			pageTitleBox.addClass("page-title-box");
@@ -73,48 +103,6 @@ public abstract class DisplayScreen<J extends DisplayScreen<J>>
 		GuiceContext.get(AjaxResponse.class)
 		            .addComponent(responsive);
 		return responsive;
-	}
-
-	@Override
-	public void preConfigure()
-	{
-		if (!isConfigured())
-		{
-			setID("content-updatable");
-			addStyle("background-color:#333");
-			addStyle("padding:0px;");
-
-			Div d = getContentContainer();
-			d.setID("content-updatable-content");
-			if(!getPage().isMobileOrSmartTablet())
-			{
-				d.addStyle("margin-top:15px");
-			}
-
-
-			BSCardBody body = addCardBody().add(d);
-			body.addStyle("padding:0px;");
-
-			SlimScrollFeature destroyFeature = new SlimScrollFeature(this);
-			destroyFeature.getOptions()
-			              .setDestroy(true);
-			addFeature(destroyFeature);
-			addFeature(new DefaultSlimScroll(this));
-
-			d.setID("innerContentContainer");
-			if(getPage().isMobileOrSmartTablet())
-			{
-				d.addStyle("padding-left", "0px");
-				d.addStyle("padding-right", "0px");
-			}
-
-			Div slimScrollerSpacer = new Div<>().setID("slimScrollSpacer");
-			slimScrollerSpacer.addStyle("height:70px;");
-			d.add(slimScrollerSpacer);
-
-			buildTitleRow();
-		}
-		super.preConfigure();
 	}
 
 	@NotNull
