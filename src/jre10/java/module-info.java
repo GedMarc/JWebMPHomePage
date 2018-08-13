6d5bbda687302615715889e94ed46834ea56e421
GedMarc <ged_marc@hotmail.com>
2018-08-13 22:31:33 +0200

    Updates to the library versions

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -59,8 +59,6 @@ module com.jwebmp.examples.demos.homepage {
 	requires java.persistence;
 	requires java.mail;
 
-	requires commons.lang3;
-
 	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
