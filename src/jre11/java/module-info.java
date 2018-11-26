2f675f00699383aee4af84e81f3c8c13bca8d137
GedMarc <ged_marc@hotmail.com>
2018-11-27 00:23:32 +0200

    Module Referencing cleanup after core shift to pure JPMS

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -10,84 +10,67 @@ import com.jwebmp.guicedinjection.interfaces.*;
 module com.jwebmp.examples.demos.homepage {
 	exports com.jwebmp.examples.demos.homepage;
 
-	requires javax.servlet.api;
-	//requires java.logging;
+	//This library is used for class scanning (https://github.com/classgraph/classgraph)
+	requires io.github.classgraph;
+	requires java.mail;
+
+	//These still require porting to jpms, otherwise they wouldn't need to be specified here
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires com.google.common;
 
 	requires undertow.core;
 	requires undertow.servlet;
+	requires cache.api;
+	requires xnio.api;
 
+	//JWebMP
+	requires com.jwebmp.entityassist;
+	requires com.jwebmp.undertow;
+
+	//JWebMP Addons
+	requires com.jwebmp.guicedservlets.requestscoped;
+	requires com.jwebmp.guicedpersistence.readers.hibernateproperties;
+	requires com.jwebmp.guicedpersistence.btm;
+
+	//JWebMP Lower Level
 	requires com.jwebmp.guicedinjection;
 	requires com.jwebmp.logmaster;
 	requires com.jwebmp.core;
-	requires com.jwebmp.guicedpersistence;
+	requires com.jwebmp.guicedservlets;
 
-	requires com.google.guice.extensions.servlet;
+	//Guice
 	requires com.google.guice;
 	requires com.google.guice.extensions.persist;
 
-	requires com.jwebmp.undertow;
-	requires com.jwebmp.websockets;
-
 	requires com.jwebmp.plugins.fontawesome5;
-	//requires com.jwebmp.components.d3.reingoldtilfordtree;
-	//requires com.jwebmp.plugins.jqueryui.themes;
 	requires com.jwebmp.plugins.bootstrap4;
-	//requires com.jwebmp.plugins.fontawesome;
 	requires com.jwebmp.plugins.google.sourceprettify;
 	requires com.jwebmp.plugins.jqlayout;
 	requires com.jwebmp.plugins.metrojs;
-
+	requires com.jwebmp.plugins.skycons;
+	requires com.jwebmp.plugins.datatable;
 	requires com.jwebmp.plugins.angularslimscroll;
 	requires com.jwebmp.plugins.jstree;
-	//requires com.jwebmp.plugins.ionic.ionicons;
-	//requires com.jwebmp.plugins.materialicons;
 	requires com.jwebmp.plugins.softhistorychange;
 	requires com.jwebmp.plugins.jqueryui;
-	//requires com.jwebmp.plugins.materialdesignicons;
-	//requires com.jwebmp.plugins.glyphicons;
 	requires com.jwebmp.plugins.moment;
 	requires com.jwebmp.plugins.pace;
 	requires com.jwebmp.plugins.particlejs;
 	requires com.jwebmp.plugins.toastr;
 	requires com.jwebmp.plugins.plusastab;
+	requires com.jwebmp.plugins.blueimp.gallery;
+	requires com.jwebmp.plugins.smartwizard4;
 
-	requires com.jwebmp.entityassist;
-
-	requires java.validation;
-	requires java.activation;
-	//requires java.xml.bind;
-	requires java.persistence;
-	requires java.mail;
-
+	//In storing visitors, we save the header information as JSON
 	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
-
-	requires java.transaction;
+	requires org.json;
 
 	requires org.hibernate.orm.core;
 	requires org.hibernate.orm.jcache;
 	requires org.hibernate.validator;
-
-	requires cache.api;
-	requires undertow.websockets.jsr;
-
-	requires xnio.api;
-
-	requires com.jwebmp.guicedpersistence.btm;
-	requires org.json;
-	requires com.jwebmp.plugins.skycons;
-	requires com.jwebmp.plugins.datatable;
-	requires com.jwebmp.guicedservlets;
-	requires com.jwebmp.plugins.blueimp.gallery;
-	requires org.apache.commons.lang3;
-	requires org.apache.commons.text;
-	requires com.jwebmp.guicedservlets.requestscoped;
-	requires com.google.common;
-	requires io.github.classgraph;
-
-	requires com.jwebmp.guicedpersistence.readers.hibernateproperties;
-	requires com.jwebmp.plugins.smartwizard4;
 
 	provides IGuiceConfigurator with DemoGuiceConfigurator;
 	provides IPage with DisplayPage;
