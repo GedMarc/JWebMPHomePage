539f9a4aca3b2a2179338522e9ebc8138ca5d821
GedMarc <ged_marc@hotmail.com>
2018-12-15 22:47:40 +0200

    Fixers

--- src/main/java/com/jwebmp/examples/demos/homepage/display/about/SPIScreen.java
+++ src/main/java/com/jwebmp/examples/demos/homepage/display/about/SPIScreen.java
@@ -44,7 +44,7 @@ public class SPIScreen
 		tabs.addTab("About", buildDefaultScreen(), true);
 		//tabs.addTab("Injection", buildInjection(), false);
 	//	tabs.addTab("*<i>Classpath</i>", buildClassPath(), false);
-		tabs.addTab("JWebMP", buildJWebMP(), false);
+	//	tabs.addTab("JWebMP", buildJWebMP(), false);
 
 		//tabs.addTab("Persistence", buildPersistence(), false);
 		card.addCardBody()
@@ -92,65 +92,6 @@ public class SPIScreen
 	}
 
 
-
-	private Div buildJWebMP()
-	{
-		DisplayCard card = new DisplayCard();
-		Div div = card.addCardBody();
-		div.add(new H3("JWebMP Service Loaders"));
-		div.add(new H4("com.jwebmp.core.services"));
-		BSTable<?> table = new BSTable<>().addTheme(BSTableOptions.Table_Dark)
-		                                  .addClass(Table_Hover);
-		table.setSmall(true);
-		table.setBordered(true);
-		table.setStriped(true);
-
-		table.add(new TableHeaderGroup<>().add(new TableRow<>().add(new TableHeaderCell<>("Service Loader"))
-		                                                       .add(new TableHeaderCell<>("Purpose"))
-		                                      ));
-
-		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IPage"))
-		                                       .add(new TableCell<>(
-				                                       "Designates a page that must be rendered. The class must extend Page.<br/> Annotate with @PageConfiguration to configure URL's")));
-		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IErrorPage"))
-		                                       .add(new TableCell<>("Allows to set a custom page to render errors on.")));
-		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IPageConfigurator"))
-		                                       .add(new TableCell<>("Configures and modifies the page before rendering as required by the given module")));
-		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IRegularExpressions"))
-		                                       .add(new TableCell<>("Supplies a list of default regular expressions to the client browser on connect for quick static access")));
-
-		div.add(table);
-
-		div.add(new H3("Angular Configuration Loaders"));
-		div.add(new H4("com.jwebmp.core.base.angular.services"));
-		BSTable<?> tableAngular = new BSTable<>().addTheme(BSTableOptions.Table_Dark)
-		                                         .addClass(Table_Hover);
-		tableAngular.setSmall(true);
-		tableAngular.setBordered(true);
-		tableAngular.setStriped(true);
-
-		tableAngular.add(new TableHeaderGroup<>().add(new TableRow<>().add(new TableHeaderCell<>("Service Loader"))
-		                                                              .add(new TableHeaderCell<>("Purpose"))
-		                                             ));
-
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularDirective"))
-		                                              .add(new TableCell<>("Adds the given Angular Directive to the page rendered to the client")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularControllerScopeStatement"))
-		                                              .add(new TableCell<>("Adds script text inside the root angular controller with direct access to the scope")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularModule"))
-		                                              .add(new TableCell<>("Adds the given angular module to the initialization of angular")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularConfigurationScopeStatement"))
-		                                              .add(new TableCell<>("Adds the given scoped statement to the angular configuration script")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularConfiguration"))
-		                                              .add(new TableCell<>("Registers a new angular configuration object to be rendered.")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularController"))
-		                                              .add(new TableCell<>("Registers the new controller to the Angular script")));
-		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularFactory"))
-		                                              .add(new TableCell<>("Adds the given factory to the script")));
-
-		div.add(tableAngular);
-		return card;
-	}
 
 
 	private Div buildPluginsPanel()
--- src/main/java/com/jwebmp/examples/demos/homepage/display/injection/InjectionsJWebMPScreen.java
+++ src/main/java/com/jwebmp/examples/demos/homepage/display/injection/InjectionsJWebMPScreen.java
@@ -1,22 +1,24 @@
 package com.jwebmp.examples.demos.homepage.display.injection;
 
-import com.jwebmp.core.base.html.Div;
-import com.jwebmp.core.base.html.H3;
-import com.jwebmp.core.base.html.Link;
-import com.jwebmp.core.base.html.SmallText;
+import com.jwebmp.core.base.html.*;
 import com.jwebmp.examples.demos.homepage.components.DefaultSlimScroll;
 import com.jwebmp.examples.demos.homepage.components.display.DefaultDisplayWizard;
+import com.jwebmp.examples.demos.homepage.components.display.DisplayCard;
 import com.jwebmp.examples.demos.homepage.components.display.DisplayScreen;
 import com.jwebmp.plugins.bootstrap4.breadcrumbs.BSBreadCrumb;
 import com.jwebmp.plugins.bootstrap4.breadcrumbs.BSBreadCrumbItem;
 import com.jwebmp.plugins.bootstrap4.containers.BSContainer;
 import com.jwebmp.plugins.bootstrap4.containers.BSRow;
+import com.jwebmp.plugins.bootstrap4.options.BSTableOptions;
+import com.jwebmp.plugins.bootstrap4.tables.BSTable;
+import com.jwebmp.plugins.bootstrap4.tables.BSTableRow;
 import com.jwebmp.plugins.smartwizard4.SmartWizardStep;
 import com.jwebmp.plugins.smartwizard4.SmartWizardStepItem;
 
 import javax.validation.constraints.NotNull;
 
 import static com.jwebmp.plugins.bootstrap4.options.BSContainerOptions.*;
+import static com.jwebmp.plugins.bootstrap4.options.BSTableOptions.*;
 
 @SuppressWarnings("Duplicates")
 public class InjectionsJWebMPScreen
@@ -33,7 +35,7 @@ public class InjectionsJWebMPScreen
 		BSContainer container = BSContainer.newInstance(Container_Fluid);
 		//container.addClass("row");
 
-		container.add(new H3<>("Injection Control<br/>"));
+		//container.add(new H3<>("Injection Control<br/>"));
 
 		Div aboutContent = new Div();
 		Div whatAvailableContainer = new Div();
@@ -53,10 +55,72 @@ public class InjectionsJWebMPScreen
 		wizard.addStep(new SmartWizardStep(pageContent, new SmartWizardStepItem("Defaults", new SmallText("Standard Events Available"))));
 		wizard.addStep(new SmartWizardStep(dataContent, new SmartWizardStepItem("Data", new SmallText("Return data from client"))));
 
-		container.add(wizard);
+		//container.add(wizard);
+		container.add(buildJWebMP());
 
 		return container;
 	}
+
+
+	private Div buildJWebMP()
+	{
+		DisplayCard card = new DisplayCard();
+		Div div = card.addCardBody();
+		div.add(new H3("JWebMP Service Loaders"));
+		div.add(new H4("com.jwebmp.core.services"));
+		BSTable<?> table = new BSTable<>().addTheme(BSTableOptions.Table_Dark)
+		                                  .addClass(Table_Hover);
+		table.setSmall(true);
+		table.setBordered(true);
+		table.setStriped(true);
+
+		table.add(new TableHeaderGroup<>().add(new TableRow<>().add(new TableHeaderCell<>("Service Loader"))
+		                                                       .add(new TableHeaderCell<>("Purpose"))
+		                                      ));
+
+		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IPage"))
+		                                       .add(new TableCell<>(
+				                                       "Designates a page that must be rendered. The class must extend Page.<br/> Annotate with @PageConfiguration to configure URL's")));
+		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IErrorPage"))
+		                                       .add(new TableCell<>("Allows to set a custom page to render errors on.")));
+		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IPageConfigurator"))
+		                                       .add(new TableCell<>("Configures and modifies the page before rendering as required by the given module")));
+		table.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IRegularExpressions"))
+		                                       .add(new TableCell<>("Supplies a list of default regular expressions to the client browser on connect for quick static access")));
+
+		div.add(table);
+
+		div.add(new H3("Angular Configuration Loaders"));
+		div.add(new H4("com.jwebmp.core.base.angular.services"));
+		BSTable<?> tableAngular = new BSTable<>().addTheme(BSTableOptions.Table_Dark)
+		                                         .addClass(Table_Hover);
+		tableAngular.setSmall(true);
+		tableAngular.setBordered(true);
+		tableAngular.setStriped(true);
+
+		tableAngular.add(new TableHeaderGroup<>().add(new TableRow<>().add(new TableHeaderCell<>("Service Loader"))
+		                                                              .add(new TableHeaderCell<>("Purpose"))
+		                                             ));
+
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularDirective"))
+		                                              .add(new TableCell<>("Adds the given Angular Directive to the page rendered to the client")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularControllerScopeStatement"))
+		                                              .add(new TableCell<>("Adds script text inside the root angular controller with direct access to the scope")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularModule"))
+		                                              .add(new TableCell<>("Adds the given angular module to the initialization of angular")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularConfigurationScopeStatement"))
+		                                              .add(new TableCell<>("Adds the given scoped statement to the angular configuration script")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularConfiguration"))
+		                                              .add(new TableCell<>("Registers a new angular configuration object to be rendered.")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularController"))
+		                                              .add(new TableCell<>("Registers the new controller to the Angular script")));
+		tableAngular.add(new BSTableRow<>(Table_Hover).add(new TableCell<>("IAngularFactory"))
+		                                              .add(new TableCell<>("Adds the given factory to the script")));
+
+		div.add(tableAngular);
+		return card;
+	}
+
 
 	@Override
 	public BSBreadCrumb<?> getTitleBreadcrumbs()
